846e5b017d43422bbb386880255a9dcabfb13832
Stanislav Liudera <stasliudera@gmail.com>
2022-11-20 17:55:43 +0100

    Add match class tests.

--- src/match.test.ts
+++ src/match.test.ts
@@ -1,4 +1,23 @@
+import { Match } from './match';
+
 describe('Test Match Class', () => {
-  it.todo('Create a new match');
-  it.todo('A new match have 0 - 0 score');
+  const homeTeamName = 'Home Team';
+  const awayTeamName = 'Away Team';
+  let match: Match;
+
+  beforeEach(() => {
+    match = new Match({
+      homeTeamName,
+      awayTeamName,
+    });
+  });
+
+  it('Create a new match', () => {
+    expect(match.homeTeamName).toBe(homeTeamName);
+    expect(match.awayTeamName).toBe(awayTeamName);
+  });
+  it('A new match have 0 - 0 score', () => {
+    expect(match.homeTeamScore).toBe(0);
+    expect(match.awayTeamScore).toBe(0);
+  });
 });
