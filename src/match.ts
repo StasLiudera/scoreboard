aebdb0e4f38406d61454fe83d6954eb6a2679d9e
Stanislav Liudera <stasliudera@gmail.com>
2022-11-20 18:29:47 +0100

    Add match class updateScore method.

--- src/match.ts
+++ src/match.ts
@@ -3,20 +3,47 @@ interface MatchConstructorProperties {
   awayTeamName: string
 }
 
+interface MatchUpdateScoreProperties {
+  homeTeamScore : number,
+  awayTeamScore: number
+}
+
 class Match {
   readonly homeTeamName: string;
 
   readonly awayTeamName : string;
 
-  readonly homeTeamScore: number;
+  private privateHomeTeamScore: number;
 
-  readonly awayTeamScore: number;
+  private privateAwayTeamScore: number;
+
+  get homeTeamScore() { return this.privateHomeTeamScore; }
+
+  get awayTeamScore() { return this.privateAwayTeamScore; }
 
   constructor({ homeTeamName, awayTeamName }: MatchConstructorProperties) {
     this.homeTeamName = homeTeamName;
     this.awayTeamName = awayTeamName;
-    this.homeTeamScore = 0;
-    this.awayTeamScore = 0;
+    this.privateHomeTeamScore = 0;
+    this.privateAwayTeamScore = 0;
+  }
+
+  updateScore({ homeTeamScore, awayTeamScore }: MatchUpdateScoreProperties) {
+    if (!Number.isInteger(homeTeamScore)) {
+      throw Error('homeTeamScore is no integer');
+    }
+    if (!Number.isInteger(awayTeamScore)) {
+      throw Error('awayTeamScore is no integer');
+    }
+    if (homeTeamScore < 0) {
+      throw Error('homeTeamScore is negative');
+    }
+    if (awayTeamScore < 0) {
+      throw Error('awayTeamScore is negative');
+    }
+
+    this.privateHomeTeamScore = homeTeamScore;
+    this.privateAwayTeamScore = awayTeamScore;
   }
 }
 
