2c138561e0ba6be96bfc0664abccf8d79e90c7bf
Stanislav Liudera <stasliudera@gmail.com>
2022-11-20 18:09:57 +0100

    Add match class.

--- src/match.ts
+++ src/match.ts
@@ -1,4 +1,23 @@
+interface MatchConstructorProperties {
+  homeTeamName : string,
+  awayTeamName: string
+}
+
 class Match {
+  readonly homeTeamName: string;
+
+  readonly awayTeamName : string;
+
+  readonly homeTeamScore: number;
+
+  readonly awayTeamScore: number;
+
+  constructor({ homeTeamName, awayTeamName }: MatchConstructorProperties) {
+    this.homeTeamName = homeTeamName;
+    this.awayTeamName = awayTeamName;
+    this.homeTeamScore = 0;
+    this.awayTeamScore = 0;
+  }
 }
 
 export default Match;
