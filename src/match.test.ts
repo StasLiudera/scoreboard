079f402b03f2bcb4a86559108cb284e38bf8b101
Stanislav Liudera <stasliudera@gmail.com>
2022-11-20 18:23:01 +0100

    Add additional match class tests for updateScore.

--- src/match.test.ts
+++ src/match.test.ts
@@ -20,4 +20,84 @@ describe('Test Match Class', () => {
     expect(match.homeTeamScore).toBe(0);
     expect(match.awayTeamScore).toBe(0);
   });
+  describe('Update match score', () => {
+    it('Update score is recorded', () => {
+      const homeTeamScore = 10;
+      const awayTeamScore = 20;
+
+      match.updateScore({
+        homeTeamScore,
+        awayTeamScore,
+      });
+
+      expect(match.homeTeamScore).toBe(homeTeamScore);
+      expect(match.awayTeamScore).toBe(awayTeamScore);
+    });
+    it('Accept Home Team Score to throw error on string value', () => {
+      const homeTeamScore = '10' as unknown as number;
+      const awayTeamScore = 20;
+
+      expect(() => {
+        match.updateScore({
+          homeTeamScore,
+          awayTeamScore,
+        });
+      }).toThrow();
+    });
+    it('Accept Away Team Score to throw error on string value', () => {
+      const homeTeamScore = 10;
+      const awayTeamScore = '20' as unknown as number;
+
+      expect(() => {
+        match.updateScore({
+          homeTeamScore,
+          awayTeamScore,
+        });
+      }).toThrow();
+    });
+    it('Accept Home Team Score to throw error on float value', () => {
+      const homeTeamScore = 10.1;
+      const awayTeamScore = 10;
+
+      expect(() => {
+        match.updateScore({
+          homeTeamScore,
+          awayTeamScore,
+        });
+      }).toThrow();
+    });
+    it('Accept Away Team Score to throw error on float value', () => {
+      const homeTeamScore = 10;
+      const awayTeamScore = 20.1;
+
+      expect(() => {
+        match.updateScore({
+          homeTeamScore,
+          awayTeamScore,
+        });
+      }).toThrow();
+    });
+    it('Does Home Team Score to throw error on negative values', () => {
+      const homeTeamScore = -10;
+      const awayTeamScore = 20;
+
+      expect(() => {
+        match.updateScore({
+          homeTeamScore,
+          awayTeamScore,
+        });
+      }).toThrow();
+    });
+    it('Does Away Team Score to throw error on negative values', () => {
+      const homeTeamScore = 10;
+      const awayTeamScore = -20;
+
+      expect(() => {
+        match.updateScore({
+          homeTeamScore,
+          awayTeamScore,
+        });
+      }).toThrow();
+    });
+  });
 });
